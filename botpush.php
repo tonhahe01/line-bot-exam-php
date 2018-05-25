f5a62b7b5a87970eebcca425d74148547a0caff8
tonhahe01 <39621978+tonhahe01@users.noreply.github.com>
2018-05-25 19:56:52 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,8 +4,7 @@
 
 require "vendor/autoload.php";
 
-$access_token = 'AAbp8VHPX8Nj3IAXk9qgMihub95O4mAfpHVvDYBbH4YJ1DDJADiIvxjaA/K0Ugr8Xq/zFQwLGzGGl4464dTZ91gxkHZYD7Og9slH1QnoTHLmLH9Am9WcNlXKs3Tu3RFVV/vyP3Ws5xUhIR12vHHgRwdB04t89/1O/w1cDnyilFU=
-';
+$access_token = 'AAbp8VHPX8Nj3IAXk9qgMihub95O4mAfpHVvDYBbH4YJ1DDJADiIvxjaA/K0Ugr8Xq/zFQwLGzGGl4464dTZ91gxkHZYD7Og9slH1QnoTHLmLH9Am9WcNlXKs3Tu3RFVV/vyP3Ws5xUhIR12vHHgRwdB04t89/1O/w1cDnyilFU=';
 
 $channelSecret = '4fef250e5992558d03def0a8ca2827b3';
 
