7dcf9aef053b615db018bd9332d585a78644ba4b
tonhahe01 <39621978+tonhahe01@users.noreply.github.com>
2018-05-28 20:16:26 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,4 +1,8 @@
 <?php
+curl -X GET \
+-H 'Authorization: Bearer {ENTER_ACCESS_TOKEN}' \
+https://api.line.me/v1/oauth/verify
+
 $access_token = 'FWAnBvc7hl9c/l96z7jXEtWqAm1JVGl+AWZz51ubTNwSiHsqWqSfZtjazFpBGo5MXq/zFQwLGzGGl4464dTZ91gxkHZYD7Og9slH1QnoTHI97ZgmEQpUWKwiVk9w5kdt76K+jcdddKyTzjF+YCaYHQdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
