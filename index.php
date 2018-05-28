c2ad0c6ad4ee920905dd67c60c266593c5fa3c52
tonhahe01 <39621978+tonhahe01@users.noreply.github.com>
2018-05-28 20:46:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -22,31 +22,4 @@ $box=array("$content");
      $tt=("Problem reading csv"); 
  }
   echo $tt;
- 
 
-$messages = [
-'type' => 'text',
-'text' => $tt
-];
-// Make a POST Request to Messaging API to reply to sender
-$url = 'https://api.line.me/v2/bot/message/reply';
-$data = [
-'replyToken' => $replyToken,
-'messages' => [$messages],
-];
-$post = json_encode($data);
-$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-echo $result . "\r\n";
-}
-}
-}
-echo "OK";
- 
