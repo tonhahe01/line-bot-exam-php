e55f6d7eaaa668ef77acadbfcca08b015f49dfdb
tonhahe01 <39621978+tonhahe01@users.noreply.github.com>
2018-05-30 11:16:24 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -84,7 +84,7 @@ if(!is_null($events)){
 							 if(!ini_set('default_socket_timeout', 15)) $tt = "<!-- unable to change socket timeout -->";
 							 if (($handle = fopen($spreadsheet_url, "r")) !== FALSE) {
 							  $tt="";
-							  $n="\r\rASTON INVENTORY <br> \r";
+							  $n="\r\rASTON INVENTORY\r";
 								 while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
 								  if(in_array($data[1],$box)){
 								   $tt.=$n.$data[1]." คงเหลือ  ".$data[11];
