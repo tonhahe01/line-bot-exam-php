e6a7cddeb477885d9f599e5ca4b53d123dcd8388
tonhahe01 <39621978+tonhahe01@users.noreply.github.com>
2018-05-31 11:04:06 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -66,89 +66,14 @@ if(!is_null($events)){
     switch ($typeMessage){
         case 'text':
             switch ($userMessage) {
-					<?php
-stream_context_set_default([
-		'ssl' => [
-			'verify_peer' => false,
-			'verify_peer_name' => false,
-		]
-]);
-
-$spreadsheet_url="https://docs.google.com/spreadsheets/d/e/2PACX-1vSPeOnhVSU6D396bjBcc_92Cm0vwS_pbeVB_-Ix_a_FXIkeCkeXr7SW-JcZHksKFHQ8YGQp2KlfgBnJ/pub?gid=1511270185&single=true&output=csv";
-$box=array("$userMessage");
-if(!ini_set('default_socket_timeout', 15)) $tt = "<!-- unable to change socket timeout -->";
-	if (($handle = fopen($spreadsheet_url, "r")) !== FALSE) {
-  $tt="";
-  $nc = "";
-  $n="\r\rASTON INVENTORY <br> \r";
-     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-      if(in_array($data[1],$box)){
-       $tt.=$n.$data[1]." คงเหลือ  ".$data[11];
-	  
-	  $nc = $data[1];
-       $n="\r <br>";	
-	  }
-     }
-	
-	
-	 $c = strlen($nc);
-		echo $c;
-
-		if($c == "0"){
-			
-								stream_context_set_default([
-										'ssl' => [
-											'verify_peer' => false,
-											'verify_peer_name' => false,
-										]
-								]);
-								$sinven = $_GET["item"];
-								$spreadsheet_url="https://docs.google.com/spreadsheets/d/e/2PACX-1vRVXnIFDOSIlOdrKAbqLbXsOfj9ZX1FpXFgRVhrUpXamvO26JTNaZTHNdCynf6fpjUn83SSVkRDCnhE/pub?gid=1511270185&single=true&output=csv";
-								$box=array("$userMessage");
-								 if(!ini_set('default_socket_timeout', 15)) $tt = "<!-- unable to change socket timeout -->";
-								 if (($handle = fopen($spreadsheet_url, "r")) !== FALSE) {
-								  $tt ="";
-								  $nc = "";
-								  $n="\r\rDENGO INVENTORY <br> \r";
-									 while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-									  if(in_array($data[1],$box)){
-									   $tt.=$n.$data[1]." คงเหลือ  ".$data[11];
-									    $nc = $data[1];
-									   $n="\r <br>";	
-									  }
-									 }
-									 
-											echo $tt;
-												
-										
-									 fclose($handle);    
-								 }
-								 else{
-									 $tt="Problem reading csv"; 
-								 }
-								
-			
-										}
-									else {
-										echo $tt;
-										}
-								
-							   
-							 }
-
-							 else{
-								 $tt="Problem reading csv"; 
-							 }
-				
-				
-				
-
-					
-				
-                    $textReplyMessage = "$tt";
+                case "A":
+                    $textReplyMessage = "คุณพิมพ์ A";
+                    break;
+                case "B":
+                    $textReplyMessage = "คุณพิมพ์ B";
                     break;
                 default:
-                    $textReplyMessage = "ไม่พบข้อมูล";
+                    $textReplyMessage = " คุณไม่ได้พิมพ์ A และ B";
                     break;                                      
             }
             break;
